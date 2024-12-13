f6152fd9d821592db9422c09fd5460eb6788482b
chai-py <er.chaital123@gmail.com>
2024-12-13 00:54:12 -0700

    added role in user model

--- model/UserModel.js
+++ model/UserModel.js
@@ -14,6 +14,11 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
     },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
 });
 const User = mongoose.model("User", userSchema);
 export default User;
